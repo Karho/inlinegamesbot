37aab2e853a3ccaf6b27d30e8ba50d36e3efec11
Karho <Karho@users.noreply.github.com>
2019-05-04 19:10:25 +0800

    Update Tictacfour.php
    Adding TTF with a revenge variant.

--- src/Entity/Game/Tictacfour.php
+++ src/Entity/Game/Tictacfour.php
@@ -10,6 +10,11 @@
 
 namespace jacklul\inlinegamesbot\Entity\Game;
 
+use jacklul\inlinegamesbot\Entity\Game;
+use jacklul\inlinegamesbot\Exception\StorageException;
+use jacklul\inlinegamesbot\Helper\Utilities;
+use Spatie\Emoji\Emoji;
+
 /**
  * Tic-Tac-Four
  */
@@ -34,7 +39,7 @@ class Tictacfour extends Tictactoe
      *
      * @var string
      */
-    protected static $description = 'Tic-tac-four is a game for two players, X and O, who take turns marking the spaces in a 7×6 grid.';
+    protected static $description = 'Tic-tac-four is a game for two players who take turns marking the spaces in a 6×5 grid. Each player chooses X or O in his turn, and may change his minds from turns to turns. The winner is the one who finishes to connect 4-in-a-row, but loses when your opponent can connect with the aternative immediately after that.';
 
     /**
      * Game thumbnail image
@@ -56,13 +61,140 @@ class Tictacfour extends Tictactoe
      * @var array
      */
     protected $board = [
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
     ];
+
+    /**
+     * Game handler
+     *
+     * @return \Longman\TelegramBot\Entities\ServerResponse|mixed
+     *
+     * @throws \jacklul\inlinegamesbot\Exception\BotException
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     * @throws \jacklul\inlinegamesbot\Exception\StorageException
+     */
+    protected function gameAction()
+    {
+        if ($this->getCurrentUserId() !== $this->getUserId('host') && $this->getCurrentUserId() !== $this->getUserId('guest')) {
+            return $this->answerCallbackQuery(__("You're not in this game!"), true);
+        }
+
+        $data = &$this->data['game_data'];
+
+        $this->defineSymbols();
+
+        $callbackquery_data = $this->manager->getUpdate()->getCallbackQuery()->getData();
+        $callbackquery_data = explode(';', $callbackquery_data);
+
+        $command = $callbackquery_data[1];
+
+        $args = null;
+        if (isset($callbackquery_data[2])) {
+            $args = explode('-', $callbackquery_data[2]);
+        }
+
+        if ($command === 'start') {
+            if (isset($data['settings']) && $data['settings']['X'] == 'host') {
+                $data['settings']['X'] = 'guest';
+                $data['settings']['O'] = 'host';
+            } else {
+                $data['settings']['X'] = 'host';
+                $data['settings']['O'] = 'guest';
+            }
+
+            $data['current_turn'] = 'X';
+            $data['board'] = $this->board;
+
+            Utilities::debugPrint('Game initialization');
+        } elseif ($args === null) {
+            Utilities::debugPrint('No move data received');
+        }
+
+        if (isset($data['current_turn']) && $data['current_turn'] == 'E') {
+            return $this->answerCallbackQuery(__("This game has ended!"), true);
+        }
+
+        if ($this->getCurrentUserId() !== $this->getUserId($data['settings'][$data['current_turn']]) && $command !== 'start') {
+            return $this->answerCallbackQuery(__("It's not your turn!"), true);
+        }
+
+        if (isset($args) && isset($data['board'][$args[0]][$args[1]]) && $data['board'][$args[0]][$args[1]] !== '') {
+            return $this->answerCallbackQuery(__("Invalid move!"), true);
+        }
+
+        $this->max_y = count($data['board']);
+        $this->max_x = count($data['board'][0]);
+
+        if (isset($args)) {
+            if ($data['current_turn'] == 'X') {
+                $data['board'][$args[0]][$args[1]] = 'X';
+                $data['current_turn'] = 'O';
+            } elseif ($data['current_turn'] == 'O') {
+                $data['board'][$args[0]][$args[1]] = 'O';
+                $data['current_turn'] = 'X';
+            } else {
+                Utilities::debugPrint('Invalid move data: ' . ($args[0]) . ' - ' . ($args[1]));
+
+                return $this->answerCallbackQuery(__("Invalid move!"), true);
+            }
+
+            Utilities::debugPrint($data['current_turn'] . ' placed at ' . ($args[1]) . ' - ' . ($args[0]));
+        }
+
+        $isOver = $this->isGameOver($data['board']);
+        $gameOutput = '';
+
+        if (!empty($isOver) && in_array($isOver, ['X', 'O'])) {
+            $isOverPenultimate = $isOver;
+            $gameOutput = Emoji::doubleExclamationMark() . ' <b>' . __("{PLAYER} probably won! Wait the revenge", ['{PLAYER}' => '</b>' . $this->getUserMention($data['settings'][$isOver]) . '<b>']) . '</b>';
+        } elseif ($isOver == 'T') {
+            $gameOutput = Emoji::chequeredFlag() . ' <b>' . __("Game ended with a draw!") . '</b>';
+        }
+        
+        if (!empty($isOver) && in_array($isOver, ['X', 'O', 'T'])) {
+            if ($isOver != 'T') {
+                $gameOutput += . '\\n Last turn: ' . $this->getUserMention($data['settings'][$data['current_turn']]) . ' (' . $this->symbols[$data['current_turn']] . ')';
+                if ($isOver != $isOverPenultimate) {
+                    $gameOutput = Emoji::trophy() . ' <b>' . __("{PLAYER} won and revenged!", ['{PLAYER}' => '</b>' . $this->getUserMention($data['settings'][$isOver]) . '<b>']) . '</b>';
+                } else {
+                    $gameOutput = Emoji::trophy() . ' <b>' . __("{PLAYER} won finally!", ['{PLAYER}' => '</b>' . $this->getUserMention($data['settings'][$isOver]) . '<b>']) . '</b>';
+                }
+            }
+            $data['current_turn'] = 'E';
+        } else {
+            $gameOutput = Emoji::blackRightwardsArrow() . ' ' . $this->getUserMention($data['settings'][$data['current_turn']]) . ' (' . $this->symbols[$data['current_turn']] . ')';
+        }
+        
+        if ($this->saveData($this->data)) {
+            return $this->editMessage(
+                $this->getUserMention('host') . ' (' . (($data['settings']['X'] == 'host') ? $this->symbols['X'] : $this->symbols['O']) . ')' . ' ' . Emoji::squaredVs() . ' ' . $this->getUserMention('guest') . ' (' . (($data['settings']['O'] == 'guest') ? $this->symbols['O'] : $this->symbols['X']) . ')' . PHP_EOL . PHP_EOL . $gameOutput,
+                $this->gameKeyboard($data['board'], $isOver)
+            );
+        } else {
+            throw new StorageException();
+        }
+    }
+
+    /**
+     * Define game symbols (emojis)
+     */
+    protected function defineSymbols()
+    {
+        $this->symbols['empty'] = '.';
+
+        $this->symbols['X'] = Emoji::crossMark();
+        $this->symbols['O'] = Emoji::heavyLargeCircle();
+
+        $this->symbols['X_won'] = $this->symbols['X'];
+        $this->symbols['O_won'] = $this->symbols['O'];
+
+        $this->symbols['X_lost'] = Emoji::heavyMultiplicationX();
+        $this->symbols['O_lost'] = Emoji::radioButton();
+    }
 
     /**
      * Check whenever game is over
